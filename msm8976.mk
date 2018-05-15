6a8d7e20edd3837b4acedf7ce89564ad177e99d5
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-15 11:33:39 +0800

    msm8976: remove duplicate package entries

--- msm8976.mk
+++ msm8976.mk
@@ -132,24 +132,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8952 \
-    libcurl
-
-PRODUCT_PACKAGES += \
-    flp.conf \
-    gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
-
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8952
